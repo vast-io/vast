f95d8d783936a1f9200de732e9a00d0e20652b59
Tobias Mayer <tobim@fastmail.fm>
2020-08-11 22:35:23 +0200

    Apply cmake-format

--- cmake/Findsystemd.cmake
+++ cmake/Findsystemd.cmake
@@ -7,45 +7,43 @@ set(systemd_VERSION ${PC_systemd_VERSION})
 if (${PC_systemd_SERVICES_INSTALL_DIR})
   set(systemd_SERVICES_INSTALL_DIR ${PC_systemd_SERVICES_INSTALL_DIR})
 else ()
-  execute_process(COMMAND "${PKG_CONFIG_EXECUTABLE}"
-      --variable=systemdsystemunitdir systemd
-      OUTPUT_VARIABLE systemd_SERVICES_INSTALL_DIR)
+  execute_process(
+    COMMAND "${PKG_CONFIG_EXECUTABLE}" --variable=systemdsystemunitdir systemd
+    OUTPUT_VARIABLE systemd_SERVICES_INSTALL_DIR)
   string(STRIP "${systemd_SERVICES_INSTALL_DIR}" systemd_SERVICES_INSTALL_DIR)
 endif ()
 
-find_path(systemd_INCLUDE_DIR
+find_path(
+  systemd_INCLUDE_DIR
   NAMES systemd/sd-bus.h systemd/sd-event.h
-  HINTS ${PC_systemd_INCLUDE_DIRS}
-)
+  HINTS ${PC_systemd_INCLUDE_DIRS})
 
 if (VAST_STATIC_EXECUTABLE)
-  list(APPEND systemd_LIB_NAMES "${CMAKE_STATIC_LIBRARY_PREFIX}systemd${CMAKE_STATIC_LIBRARY_SUFFIX}")
+  list(APPEND systemd_LIB_NAMES
+       "${CMAKE_STATIC_LIBRARY_PREFIX}systemd${CMAKE_STATIC_LIBRARY_SUFFIX}")
 else ()
   list(APPEND systemd_LIB_NAMES systemd)
 endif ()
-find_library(systemd_LIBRARY
+find_library(
+  systemd_LIBRARY
   NAMES ${systemd_LIB_NAMES}
-  HINTS ${PC_systemd_LIBRARY_DIRS}
-)
+  HINTS ${PC_systemd_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(systemd
-  FOUND_VAR
-    systemd_FOUND
-  REQUIRED_VARS
-    systemd_LIBRARY
-    systemd_INCLUDE_DIR
-  VERSION_VAR
-    systemd_VERSION
-)
+find_package_handle_standard_args(
+  systemd
+  FOUND_VAR systemd_FOUND
+  REQUIRED_VARS systemd_LIBRARY systemd_INCLUDE_DIR
+  VERSION_VAR systemd_VERSION)
 
-if(systemd_FOUND AND NOT TARGET systemd::systemd)
-    add_library(systemd::systemd UNKNOWN IMPORTED)
-    set_target_properties(systemd::systemd PROPERTIES
-        IMPORTED_LOCATION "${systemd_LIBRARY}"
-        INTERFACE_COMPILE_OPTIONS "${systemd_DEFINITIONS}"
-        INTERFACE_INCLUDE_DIRECTORIES "${systemd_INCLUDE_DIR}"
-    )
-endif()
+if (systemd_FOUND AND NOT TARGET systemd::systemd)
+  add_library(systemd::systemd UNKNOWN IMPORTED)
+  set_target_properties(
+    systemd::systemd
+    PROPERTIES
+      IMPORTED_LOCATION "${systemd_LIBRARY}" INTERFACE_COMPILE_OPTIONS
+                                             "${systemd_DEFINITIONS}"
+      INTERFACE_INCLUDE_DIRECTORIES "${systemd_INCLUDE_DIR}")
+endif ()
 
 mark_as_advanced(systemd_INCLUDE_DIR systemd_LIBRARY)
